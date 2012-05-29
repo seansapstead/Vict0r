6c9977f1a3ea7d1b324fac46187bba0e1a032bf5
Sean Sapstead <sean.sapstead@msn.com>
2012-05-29 23:05:10 +0100

    Added in exta thread to update the time on the screen to 1dp... Almost finsihed

--- Tkinter_update2.py
+++ Tkinter_update2.py
@@ -1,16 +1,38 @@
 import Tkinter as tk
-import time
-
-print time.time()
+import time, thread
 
 now = None
-
+go = None
 
 #lbl1["text"] = "HEY!"
 
+def reset():
+    
+    global now, go
+
+    now = None
+    go = None
+
+    button["text"] = "START"
+    lbl1["text"] = "Press to start"
+    
+    
+
+def threadTest():
+
+    global now, go
+
+    while go == 0:
+
+        timer = round(time.time() - now, 1)
+        
+        lbl1["text"] = timer
+        time.sleep(0.1)
+
+
 def timer():
 
-    global now
+    global now, go
 
     
     if button["text"] == "START":
@@ -18,15 +40,18 @@ def timer():
         time.sleep(0.1)
 
         button["text"] = "STOP"
+
+        go = 0
+        thread.start_new_thread(threadTest,())
         
         now = time.time()
         lbl1["text"] = "Press to stop"
-
-        ## start a new thread constently putting the time in...
-            
+               
             
     elif button["text"] == "STOP":
         button["text"] = "START"
+
+        go = 1
 
         timer = round(time.time() - now, 3)
         
@@ -41,10 +66,12 @@ lbl1.pack()
 
 root.title("Timer")
 
-root.geometry("520x125")
+root.geometry("540x160")
 
-button = tk.Button( text="START", width=30, command = timer)
+button = tk.Button( text="START", width=25, command = timer)
+button2 = tk.Button( text="Reset", width=25, command = reset)
 
 button.pack()
+button2.pack()
 
 root.mainloop()
