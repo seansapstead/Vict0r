4dac3dfe723791ca46f89ffdc0ef86b7ed5513de
Sean <sean@sean.robot>
2012-05-30 15:51:36 +0100

    removed an extra line that wasn't needed

--- Live_Ultrasound/ultrasound.py
+++ Live_Ultrasound/ultrasound.py
@@ -1,1 +1,7 @@
 pass
+
+
+## Add live streaming from ardino in another thread updating the text
+## from the main thread. This will be VERY easy to do
+
+## Add and IF into the mix so it displays ERROR rarther than the MAX number
